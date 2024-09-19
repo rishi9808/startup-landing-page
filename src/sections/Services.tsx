fc2438bb83c4ebb49e46d69ebaa089d1a7c61a94
Rishikesh K V <rishikeshkaroth@gmail.com>
2024-09-19 16:48:17 +0530

    Refactor Services section and add icons for services

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -1,4 +1,8 @@
 import { BorderBeam } from "@/components/magicui/border-beam";
+import wtspImg from "@/assets/whatsapp_3938058.png";
+import digitalMarketingIcon from "@/assets/social-media.png";
+import webDevIcon from "@/assets/web-development.png";
+import seoIcon from "@/assets/search-engine-optimization.png";
 
 const Services = () => {
   return (
@@ -7,34 +11,29 @@ const Services = () => {
         We provide..
       </h1>
       <div className="md:grid grid-cols-2 gap-20 mt-20">
-        <div className="relative flex  w-full flex-col items-center justify-center overflow-hidden rounded-lg border px-9 py-5 md:shadow-xl my-5">
-          <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-blue-200 bg-clip-text text-center text-2xl font-semibold leading-none text-transparent    ">
-            Web development
-          </span>
-          <BorderBeam size={100} duration={12} delay={9} />
+        <div className="relative flex  w-full  items-center justify-center overflow-hidden rounded-lg border px-9 py-5 md:shadow-xl my-5">
+            <img src={webDevIcon.src} alt="web" className="size-8 mr-4" />
+          <span className="text-xl font-semibold">Web development</span>
+          <BorderBeam size={80} duration={10} delay={9} />
         </div>
 
-        <div className="relative flex  w-full flex-col items-center justify-center overflow-hidden rounded-lg border px-9 py-5 md:shadow-xl my-5">
-          <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-blue-200 bg-clip-text text-center text-2xl font-semibold leading-none text-transparent    ">
-            Digital Marketing
-          </span>
-          <BorderBeam size={100} duration={12} delay={9} />
+        <div className="relative flex  w-full  items-center justify-center overflow-hidden rounded-lg border px-9 py-5 md:shadow-xl my-5">
+            <img src={seoIcon.src} alt="seo" className="size-8 mr-4" />
+          <span className="text-xl font-semibold">SEO optimization</span>
+          <BorderBeam size={80} duration={10} delay={9} />
         </div>
 
-        <div className="relative flex  w-full flex-col items-center justify-center overflow-hidden rounded-lg border px-9 py-5 md:shadow-xl my-5">
-          <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-blue-200 bg-clip-text text-center text-2xl font-semibold leading-none text-transparent    ">
-            SEO Optimization
-          </span>
-          <BorderBeam size={100} duration={12} delay={9} />
+        <div className="relative flex  w-full  items-center justify-center overflow-hidden rounded-lg border px-9 py-5 md:shadow-xl my-5">
+            <img src={digitalMarketingIcon.src} alt="digital" className="size-8 mr-4" />
+          <span className="text-xl font-semibold">Digital marketing</span>
+          <BorderBeam size={80} duration={10} delay={9} />
         </div>
 
-        <div className="relative flex  w-full flex-col items-center justify-center overflow-hidden rounded-lg border px-9 py-5 md:shadow-xl my-5">
-          <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-blue-200 bg-clip-text text-center text-2xl font-semibold leading-none text-transparent    ">
-            Whatsapp Marketing
-          </span>
-          <BorderBeam size={100} duration={12} delay={9} />
+        <div className="relative flex  w-full  items-center justify-center overflow-hidden rounded-lg border px-9 py-5 md:shadow-xl my-5 ">
+          <img src={wtspImg.src} alt="wtsp" className="size-8 mr-4" />
+          <span className="text-xl font-semibold">Whatsapp marketing</span>
+          <BorderBeam size={80} duration={10} delay={9} />
         </div>
-
       </div>
     </div>
   );
